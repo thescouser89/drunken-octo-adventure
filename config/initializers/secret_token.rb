734cfbf7165cbf14249cfc3e92d7996437ca24b3
Dustin Kut Moy Cheung <dcheung@redhat.com>
2014-03-05 14:16:41 -0500

    Initial ett4 port commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ett4::Application.config.secret_key_base = '0f0e62a258fcb7ff918913499764db11fb69670954ca8deed56c734311e666253e118dd9c41843fb4acccfb6f579680fa9440796c9f6b43e546dfed135348dca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ett4::Application.config.session_store :cookie_store, key: '_ett4_session'
